c8c5bebddd575d49e0379f6b7f9c653070baf280
sanjib-12 <sanjibbarakoti112@gmail.com>
2025-04-11 18:24:09 -0400

    Added auth subapp

--- auth/src/bootstrap.js
+++ auth/src/bootstrap.js
@@ -4,9 +4,9 @@ import App from './App';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 
 // Mount function to start up the app
-const mount = (el,{ onNavigate, defaultHistory, initialPath }) =>{
+const mount = (el,{ onSignIn, onNavigate, defaultHistory, initialPath }) =>{
    const history = defaultHistory || createMemoryHistory({
-      initialEntries: [initialPath]
+      initialEntries: [initialPath],
    });
 
    if(onNavigate){
@@ -14,7 +14,7 @@ const mount = (el,{ onNavigate, defaultHistory, initialPath }) =>{
    }
 
    ReactDOM.render(
-      <App history={history} />,
+      <App onSignIn={onSignIn} history={history} />,
       el
    )
 
@@ -32,7 +32,7 @@ const mount = (el,{ onNavigate, defaultHistory, initialPath }) =>{
 // IF we are in development and in isolation,
 // call mount immediately
 if(process.env.NODE_ENV==='development'){
-   const devRoot = document.querySelector('#_marketing-dev-root')
+   const devRoot = document.querySelector('#_auth-dev-root')
    if(devRoot){
       mount(devRoot,{defaultHistory: createBrowserHistory() });
    }
--- container/src/components/AuthApp.js
+++ container/src/components/AuthApp.js
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect } from 'react';
 import { useHistory} from 'react-router-dom';
 
-import { mount } from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 
-export default () =>{
+export default ({onSignIn}) =>{
    const ref = useRef(null);
    const history = useHistory();
 
@@ -16,7 +16,9 @@ export default () =>{
             if(pathname !== nextPathname){
                history.push(nextPathname);
             }
-         }
+         },
+
+         onSignIn,
       })
       history.listen( onParentNavigate );
    }, []);
